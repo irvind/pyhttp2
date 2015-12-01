cd4194028eed06a452210e0e5ee23cb1fc597191
Eugene Obukhov <irvind25@gmail.com>
2015-12-02 02:24:02 +0300

    all tests for utils module

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,8 @@
 import unittest
 
 from pyhttp2.utils import (
-    chunkify, rchunkify, to_bin, to_hex, byterepr_to_bytes
+    chunkify, rchunkify, to_bin, to_hex, byterepr_to_bytes, int_to_byte, byte_to_int,
+    int_to_bytes
 )
 
 
@@ -13,12 +14,24 @@ class TestUtils(unittest.TestCase):
         ])
 
         self.assertEqual(chunkify(b'123456789', 4), [b'1234', b'5678', b'9'])
+        self.assertEqual(chunkify(bytearray(b'1234'), 2), [bytearray(b'12'), bytearray(b'34')])
+        self.assertEqual(chunkify((1, 2, 3, 4), 2), [(1, 2), (3, 4)])
 
         with self.assertRaises(ValueError):
             chunkify([1, 2, 3, 4], 0)
 
     def test_rchunkify(self):
-        pass
+        self.assertEqual(rchunkify([], 5), [])
+        self.assertEqual(rchunkify(list(range(11)), 3), [
+            [0, 1], [2, 3, 4], [5, 6, 7], [8, 9, 10]
+        ])
+
+        self.assertEqual(rchunkify(b'123456789', 4), [b'1', b'2345', b'6789'])
+        self.assertEqual(rchunkify(bytearray(b'12345'), 2), [bytearray(b'1'), bytearray(b'23'), bytearray(b'45')])
+        self.assertEqual(rchunkify((1, 2, 3, 4, 5), 2), [(1,), (2, 3), (4, 5)])
+
+        with self.assertRaises(ValueError):
+            rchunkify([1, 2, 3, 4], 0)
 
     def test_to_bin(self):
         self.assertEqual(to_bin(4), '00000100')
@@ -33,7 +46,18 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(to_hex(-4), '04')
 
     def test_byte_int_convertions(self):
-        pass
+        self.assertTrue(int_to_byte(0) == b'\x00')
+        self.assertTrue(int_to_byte(16) == b'\x10')
+        self.assertTrue(int_to_byte(33) == b'\x21')
+
+        self.assertEqual(byte_to_int(int_to_byte(0)), 0)
+        self.assertEqual(byte_to_int(int_to_byte(16)), 16)
+        self.assertEqual(byte_to_int(int_to_byte(33)), 33)
+
+        self.assertEqual(int_to_bytes(0, n=3), b'\x00\x00\x00')
+        self.assertEqual(int_to_bytes(256, n=3), b'\x00\x01\x00')
+        self.assertEqual(int_to_bytes(256, n=1), b'\x00')
+        self.assertEqual(int_to_bytes(95000, n=3), b'\x01\x73\x18')
 
     def test_byterepr_to_bytes(self):
         self.assertEqual(byterepr_to_bytes('00000000'), b'\x00')
