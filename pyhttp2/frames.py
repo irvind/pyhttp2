602ec95bc778599751133c51bb9a3c6b3ca6f7d3
Eugene Obukhov <irvind25@gmail.com>
2015-12-05 19:39:49 +0300

    added frame classes which represent HEADER and DATA HTTP/2 frames

--- pyhttp2/frames.py
+++ pyhttp2/frames.py
@@ -1,42 +1,138 @@
-from pyhttp2.utils import int_to_bytes
+from .utils import int_to_bytes, int_to_byte, concat_bytes
+from .hpack import Encoder
 
 
-class FrameType(object):
-    DATA = b'\x00'
-    HEADERS = b'\x01'
-
-
-class FrameFlags(object):
-    def to_byte(flags):
-        pass
-
+# Length 24 
+# Type 8
+# Flags 8
+# R 1
+# Stream ID 31
+#######
+# Payload
 
 class Frame(object):
-    def __init__(self, _type, flags, payload=None, stream_id=None):
-        self.type = _type
+    frame_type = None
+
+    def __init__(self, stream_id, flags=None, payload=None, frame_type=None):
+        if isinstance(stream_id, int):
+            stream_id = int_to_bytes(stream_id, 4)
+        elif not isinstance(stream_id, (bytes, bytearray)):
+            raise ValueError()
+
+        self.stream_id = bytearray(stream_id)
+        self.stream_id[0] = self.stream_id[0] & 127
+
         self.flags = flags
         self.payload = payload
+        if frame_type:
+            self.frame_type = frame_type
 
-        if stream_id is None:
-            pass
+    def make_payload(self):
+        if self.payload is None:
+            raise Exception()
+
+        return bytes(self.payload)
+
+    def make_frame(self):
+        if not self.frame_type:
+            raise Exception()
+
+        # if payload is None:
+        #     payload = self.payload
+
+        # payload_len = len(payload)
+
+        payload = self.make_payload()
+        payload_len = len(payload)
+
+        return concat_bytes(
+            int_to_bytes(payload_len, 3),
+            self.frame_type,
+            self.flags if self.flags is not None else b'\x00',
+            self.stream_id,
+            payload
+        )
         
-    def bytes_repr():
-        arr = bytearray()
-        arr.extend(int_to_bytes(len(self.payload), 3))
-        arr.extend(frame_type)
 
-    def 
+class DataFrame(Frame):
+    frame_type = b'\x00'
+
+    def __init__(self, data, end_stream=False, padding=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.data = data
+        self.padding = padding
+
+        flags = 0
+        if end_stream:
+            flags |= 0x1
+        if padding:
+            flags |= 0x8
+        self.flags = int_to_byte(flags)
+
+    def make_payload(self):
+        return concat_bytes(
+            int_to_byte(self.padding) if self.padding else None,
+            self.data,
+            b'\x00' * self.padding if self.padding else None
+        )
 
 
-class HeadersFrame(object):
-    def __init__(self, *args, **kwargs):
-        super().__init__(FrameType.HEADERS, *args, **kwargs)
+# Pad length 8? (if padded)
+# E 1? (stream dependency is exclusive) (if priority)
+# Stream Dependency 31? (if priority)
+# Weight 8? (if priority)
+# Header Block Fragment *
+# Padding *
+        
+class HeadersFrame(Frame):
+    frame_type = b'\x01'
+
+    def __init__(self, headers, encoder=None, end_stream=False, end_headers=False, 
+                 padding=None, stream_dep=None, weight=None, exclusive=None,
+                 *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self.headers = headers
+        self.padding = padding
+        self.stream_dep = stream_dep
+        self.weight = weight
+        self.exclusive = exclusive
+
+        if encoder is None:
+            self.encoder = Encoder()
+
+        flags = 0
+        if end_stream:
+            flags |= 0x1
+        if end_headers:
+            flags |= 0x4
+        if padding:
+            flags |= 0x8
+        if stream_dep:
+            flags |= 0x20
+        self.flags = int_to_byte(flags)
+
+    def make_payload(self):
+        return concat_bytes(
+            int_to_byte(self.padding) if self.padding else None,
+            self._make_dep() if self.stream_dep else None
+            self.encoder.encode_headers(self.headers),
+            b'\x00' * self.padding if self.padding else None
+        )
+
+    def _make_dep(self):
+        dep = int_to_bytes(self.stream_dep, 4)
+        if self.exclusive:
+            dep[0] |= 128
+        else:
+            dep[0] &= 127
+
+        weight = int_to_byte(self.weight)
+
+        return dep, weight
 
 
-def make_frame(frame_type, flags, stream_id, payload):
-    arr = bytearray()
-    arr.extend(int_to_bytes(len(payload), 3))
-    arr.extend(frame_type)
-
-
+class PushPromiseFrame(Frame):
     pass
